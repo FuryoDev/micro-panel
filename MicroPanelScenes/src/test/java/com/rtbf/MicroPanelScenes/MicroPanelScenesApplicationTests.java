7c967e3c7b8044447ce64740e1ee92fa8de610c8
Solaiman Moujib <99276006+FuryoDev@users.noreply.github.com>
2025-11-24 13:35:52 +0100

    Added backend side

---
+++ MicroPanelScenes/src/test/java/com/rtbf/MicroPanelScenes/MicroPanelScenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtbf.MicroPanelScenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPanelScenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- vite.config.ts
+++ vite.config.ts
@@ -27,9 +27,8 @@ function createProxyConfig(apiTarget: string, cameraTarget: string) {
 }
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const apiTarget = env.VITE_PROXY_TARGET || env.VITE_API_BASE_URL || 'http://10.41.40.130:1234'
-  const cameraTarget = env.VITE_CAMERA_TARGET || 'http://10.41.39.153'
+  const apiTarget =  'http://10.41.40.130:1234'
+  const cameraTarget =  'http://10.41.39.153'
 
   return {
     plugins: [vue()],
