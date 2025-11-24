9be6b5736e0dc8ba65942108a292a18c188391cc
Solaiman Moujib <99276006+FuryoDev@users.noreply.github.com>
2025-11-24 15:16:17 +0100

    Fix backend application package and configuration scan

---
+++ MicroPanelScenes/src/test/java/com/example/micropanel/MicroPanelScenesApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.micropanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPanelScenesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
